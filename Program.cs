a15f11821d0e93c88dd839cbc50296585f7ecc1b
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2021-05-12 03:13:20 +1200

    Got memory reading working with lots of help from Livesplit

--- Program.cs
+++ Program.cs
@@ -7,9 +7,14 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using LiveSplit.ComponentUtil;
 
+
+
 namespace cotdg_mod
 {
+    using OffsetT = Int32;
     class Program {
+
+        
         const int PROCESS_WM_READ = 0x0010;
 
         [DllImport("kernel32.dll")]
@@ -19,24 +24,59 @@ namespace cotdg_mod
         public static extern bool ReadProcessMemory(int hProcess, 
         int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
         
-        
+        private static dynamic GetValue(Process p, string type, DeepPointer pointer)
+        {
+            switch (type)
+            {
+                case "int":
+                    return pointer.Deref<int>(p);
+                case "uint":
+                    return pointer.Deref<uint>(p);
+                case "long":
+                    return pointer.Deref<long>(p);
+                case "ulong":
+                    return pointer.Deref<ulong>(p);
+                case "float":
+                    return pointer.Deref<float>(p);
+                case "double":
+                    return pointer.Deref<double>(p);
+                case "byte":
+                    return pointer.Deref<byte>(p);
+                case "sbyte":
+                    return pointer.Deref<sbyte>(p);
+                case "short":
+                    return pointer.Deref<short>(p);
+                case "ushort":
+                    return pointer.Deref<ushort>(p);
+                case "bool":
+                    return pointer.Deref<bool>(p);
+                default:
+                    if (type.StartsWith("string"))
+                    {
+                        var length = int.Parse(type.Substring("string".Length));
+                        return pointer.DerefString(p, length);
+                    }
+                    else if (type.StartsWith("byte"))
+                    {
+                        var length = int.Parse(type.Substring("byte".Length));
+                        return pointer.DerefBytes(p, length);
+                    }
+                    break;
+            }
+
+            throw new ArgumentException($"The provided type, '{type}', is not supported");
+        }
         public static void Main()
         {
-            Process process = Process.GetProcessesByName("Curse of the Dead Gods")[0]; 
-            IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id); 
-
-            int bytesRead = 0;
-            byte[] buffer = new byte[24]; //'Hello World!' takes 12*2 bytes because of Unicode 
-
-
-            // 0x0046A3B8 is the address where I found the string, replace it with what you found
-            ReadProcessMemory((int)processHandle, 0x01206F30, buffer, buffer.Length, ref bytesRead);
-
-            Console.WriteLine(Encoding.Unicode.GetString(buffer) + 
-            " (" + bytesRead.ToString() + "bytes)");
-            Console.ReadLine();
-
-            
+            Process process = Process.GetProcessesByName("Curse of the Dead Gods")[0];
+            int[] offsets = {0xD8, 0x1150};
+            OffsetT _base = 0x01206F30;
+            IntPtr ptr = process.MainModuleWow64Safe().BaseAddress + _base;
+            DeepPointer IGT = new DeepPointer(0x01206F30, offsets);
+            Console.WriteLine(GetValue(process, "int", IGT));
+            IntPtr writer;
+            IGT.DerefOffsets(process, out writer);
+            process.WriteValue(writer, 100);
         }
     }
 }
